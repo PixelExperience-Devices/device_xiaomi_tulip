c9947c105b6ed72b3557fde06c8ead0286489dee
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-19 10:44:19 +0000

    tulip: Update model name and tulip product is launched with o-mr1

--- mk_tulip.mk
+++ mk_tulip.mk
@@ -17,7 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/tulip.mk)
 
 # Inherit from tulip device
@@ -30,7 +30,7 @@ PRODUCT_NAME := mk_tulip
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := tulip
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Redmi Note 5
+PRODUCT_MODEL := Redmi Note 6 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 

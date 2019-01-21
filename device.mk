02a19322370e21b6197344b1b34c2ab345c3e5e6
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    whyred: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_tulip
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \

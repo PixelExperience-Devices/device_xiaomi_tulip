b8551bb44bf526ab408e2f2d5d2c3d280f6686b0
Rahul Sehgal <47705227+rahul9810@users.noreply.github.com>
2019-03-30 08:51:41 +0530

    tulip: Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
    Fix device error on boot on other roms
    
    Change-Id: I552b90ceaaf1e656dc8b91d1fb79520452f255e3

--- mk_tulip.mk
+++ mk_tulip.mk
@@ -37,7 +37,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="tulip" \
     PRODUCT_NAME="tulip" \
-    BUILD_FINGERPRINT="xiaomi/tulip/tulip:8.1.0/OPM1.171019.011/V10.2.2.0.OEKMIXM:user/release-keys" \
     PRIVATE_BUILD_DESC="tulip-user 8.1.0 OPM1.171019.011 V10.2.2.0.OEKMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := xiaomi/tulip/tulip:8.1.0/OPM1.171019.011/V10.2.2.0.OEKMIXM:user/release-keys
 
 TARGET_VENDOR := Xiaomi

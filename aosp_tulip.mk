1453160f1fb0fe7dd6307f82fc529077662bcbd0
darkbeast69 <guptaaryan189@gmail.com>
2019-04-22 12:04:18 +0000

    tulip: revert back to 10.2.2.0.OEKMIXM

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -38,9 +38,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="tulip" \
     PRODUCT_NAME="tulip" \
-    PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 9.4.12 release-keys"
+    PRIVATE_BUILD_DESC="tulip-user 8.1.0 OPM1.171019.011 10.2.2.0 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/tulip/tulip:9/PKQ1.180904.001/9.4.12:user/release-keys
+BUILD_FINGERPRINT := xiaomi/tulip/tulip:8.1.0/OPM1.171019.011/V10.2.2.0.OEKMIXM:user/release-keys
 
 TARGET_VENDOR := Xiaomi

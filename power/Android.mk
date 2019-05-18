0c981c51c39fb88cb46c11de77a481a28fa530d9
YumeMichi <do4suki@gmail.com>
2019-05-18 16:59:09 +0000

    tulip: power: Add back DT2W support
    original commit name: [PATCH] sdm845-common: power-libperfmgr: Support DT2W
    original commit message: * Port from cdd2d45 ("sdm845-common: Switch to more generic dt2w node")
    
    * adapt to tulip powerHAL
    
    Co-authored-by: Rama Bondan Prakoso <ramanarubp@gmail.com>
    Change-Id: I8d8fcfa8b41a5a720c859dccc8680c3561e94552

--- power/Android.mk
+++ power/Android.mk
@@ -4,10 +4,15 @@ LOCAL_PATH := $(call my-dir)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common

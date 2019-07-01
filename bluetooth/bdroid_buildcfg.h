fa7fd4b546a0b95e6cc837a17459421f29ba3108
darkbeast69 <guptaaryan189@gmail.com>
2019-07-01 10:00:50 +0000

    tulip: bluetooth: Set proper model name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -30,10 +30,10 @@ static inline const char* BtmGetDefaultName()
     char product_model[PROPERTY_VALUE_MAX];
     property_get("ro.product.model", product_model, "");
 
-    if (strstr(product_model, "Redmi Note 5"))
-        return "Redmi Note 5";
-    if (strstr(product_model, "Redmi Note 5 Pro"))
-        return "Redmi Note 5 Pro";
+    if (strstr(product_model, "Redmi Note 6"))
+        return "Redmi Note 6";
+    if (strstr(product_model, "Redmi Note 6 Pro"))
+        return "Redmi Note 6 Pro";
 
     // Fallback to ro.product.model
     return "";

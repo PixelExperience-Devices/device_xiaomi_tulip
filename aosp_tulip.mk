4793190917b97344594c5a221b29df1af584b119
jhenrique09 <jhsv09@gmail.com>
2022-01-19 03:41:17 +0000

    tulip: Enable quick tap
    Change-Id: I86135ea3c4fb9063f5158f4f7ddd2d7e0f6b8062
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -13,6 +13,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from tulip device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
